d45906f9d21fb6c2f748c1322ee30128ce91d98e
Subalakshmi S <subavasagan@gmail.com>
2021-08-20 16:38:19 +0530

    Initial checkin

---
+++ src/main/java/com/suba/devops/suba_maven/App.java
@@ -0,0 +1,13 @@
+package com.suba.devops.suba_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
